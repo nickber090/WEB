7ba893122f783a46100e9255c3b04090ae36e07c
kolmakov-rodion <kolmakovrodion36@gmail.com>
2025-04-22 17:39:26 +0300

    Jinja test

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+
 
 app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/main')
+def main_window():
+    return render_template('static/templates/index.html', title='job well done')
 
 
 if __name__ == '__main__':
